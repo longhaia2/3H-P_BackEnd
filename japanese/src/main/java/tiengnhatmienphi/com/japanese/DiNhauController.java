c1b297dfbe1fe32875420bb7ce45501c87dc3599
longhaia2 <63535370+longhaia2@users.noreply.github.com>
2021-04-11 01:08:56 +0700

    Delete japanese directory

--- japanese/src/main/java/tiengnhatmienphi/com/japanese/DiNhauController.java
+++
@@ -1,9 +0,0 @@
-package tiengnhatmienphi.com.japanese;
-
-public class DiNhauController {
-
-    // 10h15 HungDT da o day
-
-    //10h30 Hien di tra sua
-    //10h50 Hien di nhau
-}
--- japanese/src/main/java/tiengnhatmienphi/com/japanese/Repository/RoomChallengeRepo.java
+++
@@ -1,8 +0,0 @@
-package tiengnhatmienphi.com.japanese.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tiengnhatmienphi.com.japanese.Entity.roomchallenge;
-
-public interface RoomChallengeRepo extends JpaRepository<roomchallenge, Integer> {
-
-}
